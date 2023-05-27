578ce6c37619902e72d9770e7e5f24a48be2c7d5
Olena Hrechykhina <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-05-27 13:54:02 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import { Profile } from 'components/Profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import { Profile } from 'components/Profile/profileee';
+import { Statistics } from 'components/Statistics/statisticsee';
+import { FriendList } from 'components/FriendList/friendlistee';
+import { TransactionHistory } from 'components/TransactionHistory/transactionhistoryee';
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
